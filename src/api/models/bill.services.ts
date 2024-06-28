188a453c61a3008fcefe262c742f03a896f27a5c
huydn04 <luckyguy52431@gmail.com>
2024-06-28 19:34:53 +0700

    feat: add vercel deploy router

--- src/api/models/bill.services.ts
+++ src/api/models/bill.services.ts
@@ -1,7 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 import { IBill } from "../types/bill.dto";
-import { enumStatus } from "~/contans/enum/bill/bill";
-
+import { enumStatus } from "../../contans/enum/bill/bill";
 
 const userSchema = new Schema<IBill>({
   _id: { type: String, required: true },
@@ -11,7 +10,17 @@ const userSchema = new Schema<IBill>({
   email: { type: String, required: true },
   total_payment: { type: Number, required: true },
   total_payment_sale: { type: Number, required: true },
-  status: {type: String ,enum: [enumStatus.PENDING, enumStatus.PAID, enumStatus.CANCELLED,enumStatus.REFUNDED],default:enumStatus.PENDING,required: true,},
+  status: {
+    type: String,
+    enum: [
+      enumStatus.PENDING,
+      enumStatus.PAID,
+      enumStatus.CANCELLED,
+      enumStatus.REFUNDED,
+    ],
+    default: enumStatus.PENDING,
+    required: true,
+  },
   phone: { type: Number, required: true },
   payment_methods: { type: Number, required: true },
   address: { type: String, required: true },
